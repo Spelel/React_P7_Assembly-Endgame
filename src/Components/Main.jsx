5dd95938012588b33a539e25003474346153586d
Arek <arek-karczo.wp.pl@wp.pl>
2026-01-05 20:20:55 +0100

    Initial commit

---
+++ src/Components/Main.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import Header from "./Header.jsx"
+export default function Main () {
+    return<>
+        <section className="flex justify-center items-start h-screen bg-[#323232]">
+           <Header />
+        </section>
+    </>
+}
